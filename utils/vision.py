a6edcf811c74a41e0adf6f86de0932e6c144e2ae
Filip Noworolnik <fnoworolnik@gmail.com>
2024-09-29 10:41:02 +0200

    Add: face pose estimation

--- utils/vision.py
+++ utils/vision.py
@@ -7,6 +7,10 @@ import mediapipe as mp
 import nltk
 import numpy as np
 from deepface import DeepFace
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 from nltk.tokenize import sent_tokenize
 
 # TODO: Remove this ugly thing :(
@@ -378,3 +382,72 @@ def compare_subtitles(video_path: Path, transcription_text: str):
     }
 
     return similarity_scores
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    face_landmarks_list = detection_result.face_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected faces to visualize.
+    for idx in range(len(face_landmarks_list)):
+        face_landmarks = face_landmarks_list[idx]
+
+        # Draw the face landmarks.
+        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        face_landmarks_proto.landmark.extend(
+            [
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x, y=landmark.y, z=landmark.z
+                )
+                for landmark in face_landmarks
+            ]
+        )
+
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style(),
+        )
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_contours_style(),
+        )
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_IRISES,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_iris_connections_style(),
+        )
+
+    return annotated_image
+
+
+def detect_face_landmarks(img_path):
+    # STEP 2: Create an FaceLandmarker object.
+    base_options = python.BaseOptions(
+        model_asset_path="face_landmarker_v2_with_blendshapes.task"
+    )
+    options = vision.FaceLandmarkerOptions(
+        base_options=base_options,
+        output_face_blendshapes=True,
+        output_facial_transformation_matrixes=True,
+        num_faces=1,
+    )
+    detector = vision.FaceLandmarker.create_from_options(options)
+
+    # STEP 3: Load the input image.
+    image = mp.Image.create_from_file(img_path)
+
+    # STEP 4: Detect face landmarks from the input image.
+    detection_result = detector.detect(image)
+
+    # STEP 5: Process the detection result. In this case, visualize it.
+    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+
+    return detection_result, annotated_image
